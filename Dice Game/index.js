6af7c3350950476772d8fa05c1983310f4754f11
aditiAJ <37775370+aditiAJ@users.noreply.github.com>
2019-03-31 15:14:01 +0530

    1st commit

---
+++ Dice Game/index.js
@@ -0,0 +1,27 @@
+var min = 1;
+var max = 6;
+var randomNumber1 =  ((Math.random() * 6) + 1);
+var randomNumber2 = Math.round(randomNumber1);
+var randomDice = "dice" + randomNumber2 + ".png";
+var randomImageSource = "images/" + randomDice;
+`var image1 = document.querySelectorAll("img")[0];
+`image1.setAttribute("src", randomImageSource);
+
+var randomNumber3 =  ((Math.random() * 6) + 1);
+var randomNumber4 = Math.round(randomNumber3);
+var randomDice = "dice" + randomNumber4 + ".png";
+var randomImageSource1 = "images/" + randomDice;
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImageSource1);
+
+if (randomNumber2 > randomNumber4) {
+  document.querySelectorAll("h1").innerHTML = "Player 1 Wins";
+}
+
+else if (randomNumber4 > randomNumber2) {
+  document.querySelectorAll("h1").innerHTML = "Player 2 Wins";
+}
+
+else {
+  document.querySelectorAll("h1").innerHTML = "Draw";
+}
